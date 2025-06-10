ec90784ba08cb91d5aafabc36ba1596c02c992f8
Cacutss <cuentasmurfdeleague@gmail.com>
2025-06-10 18:34:56 -0300

    functionality done, need info_queue

--- main.lua
+++ main.lua
@@ -30,14 +30,14 @@ SMODS.Joker {
 	    hands = 1,
 	    hand_size = 0,
 	    jokers ={
+		['j_diet_cola']=true,
 		['j_gros_michel']=true,
 		['j_ice_cream']=true,
 		['j_turtle_bean']=true,
 		['j_popcorn']=true,
 		['j_ramen']=true,
-		['j_mr_bones']=true,
 		['j_flower_pot']=true,
-		['j_chicot']=true
+		['j_mr_bones']=true
 	    }
 	}
     },
@@ -59,51 +59,72 @@ SMODS.Joker {
     blueprint_compat = true,
     pos = {x=0,y=0},
     calculate = function(self,card,context)
-	if context.setting_blind then
-	    local last_joker = nil
-	    for i,value in ipairs(G.jokers.cards) do
-	    	if card.ability.extra.jokers[value.config.center_key] then
-		    last_joker = value 
-		end
-		if value.config.center_key == "j_diet_cola" then
-		    last_joker = value
-		    if G.jokers.config.card_limit > #G.jokers.cards-1 then
-		    	copy = copy_card(card)
-			G.jokers.cards[#G.jokers.cards+1] = copy
+	local last_joker = nil
+	if context.blueprint and context.joker_main then
+	    -- only eat blueprint, bro got too close
+	    if context.blueprint_card.config.center_key == "j_blueprint" then
+		last_joker = context.blueprint_card
+	    end
+	end
+	-- on round start check the jokers for food, stop on first foodie
+	if context.setting_blind then 
+	    if not context.blueprint and last_joker == nil then
+		for i,value in ipairs(G.jokers.cards) do
+		    if last_joker ~= nil then break end
+		    if (card.ability.extra.jokers[value.config.center_key] or 
+		    (value.config.center_key == "j_Gubby_gubby" and 
+		    value.ability.extra.hand_size < card.ability.extra.hand_size))
+		    and value.locked == nil then
+			value.locked = true
+			last_joker = value 
 		    end
 		end
-		if last_joker ~= nil then 
-		    
-		    G.E_MANAGER:add_event(Event({
-			func = function()
-			    play_sound('tarot1')
-			    last_joker.T.r = -0.2
-			    last_joker:juice_up(0.3, 0.4)
-			    last_joker.states.drag.is = true
-			    last_joker.children.center.pinch.x = true
-			    -- This part destroys the card.
-			    G.E_MANAGER:add_event(Event({
-				    trigger = 'after',
-				    delay = 0.3,
-				    blockable = false,
-				    func = function()
-					    G.hand:change_size(card.ability.extra.hands)
-					    play_sound('Gubby_yummers')
-					    G.jokers:remove_card(last_joker)
-					    last_joker:remove()
-					    last_joker= nil
-					    return true;
-				    end
-			    }))
-			    return true	
-		    end				
-		    }))
-		    card.ability.extra.hand_size = card.ability.extra.hand_size + 1
-		    return{
-			    message = "Yummers!"
-		    }
-	    	end
 	    end
-	end	
+	end
+	if last_joker ~= nil then 
+	    G.E_MANAGER:add_event(Event({
+		func = function()
+		    play_sound('tarot1')
+		    last_joker.T.r = -0.2
+		    last_joker:juice_up(0.3, 0.4)
+		    last_joker.states.drag.is = true
+		    last_joker.children.center.pinch.x = true
+		    -- This part destroys the card.
+		    G.E_MANAGER:add_event(Event({
+			trigger = 'after',
+			delay = 0.3,
+			blockable = false,
+			func = function()
+			    G.hand:change_size(card.ability.extra.hands)
+			    play_sound('Gubby_yummers',pseudorandom('gubby')*3)   
+			    if last_joker.config.center_key == "j_diet_cola"then
+				if G.jokers.config.card_limit > #G.jokers.cards-1 then
+				    G.E_MANAGER:add_event(Event({
+					func = function()
+					    SMODS.add_card({
+						set = 'Joker',
+						area = G.jokers,
+						key = "j_Gubby_gubby"
+					    })
+					    return true
+					end
+				    }))
+				end
+			    end
+			    G.jokers:remove_card(last_joker)
+			    last_joker:remove()
+			    last_joker= nil
+			    return true
+			end
+		    }))
+		    return true
+		end
+	    }))
+	    card.ability.extra.hand_size = card.ability.extra.hand_size + 1
+	    return{
+		message = "Yummers!",
+		message_card = card
+	    }
+	end
     end
 }
